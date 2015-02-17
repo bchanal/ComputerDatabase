cfded6a7e7480840b9bb59f61cb85ae6603a1922
Bérangère Chanal <bchanal@excilys.com>
2015-02-17 16:34:18 +0100

    dto

---
+++ src/main/java/com/excilys/cdb/dto/ComputerDto.java
@@ -0,0 +1,71 @@
+package com.excilys.cdb.dto;
+
+import com.excilys.cdb.model.Company;
+import com.excilys.cdb.model.Computer;
+
+public class ComputerDto {
+	private int id;
+	private String name;
+	private String introduced;
+	private String discontinued;
+	private Company company;
+
+	public ComputerDto(Computer computer) {
+        super();
+        this.id = computer.getId();
+        this.name = computer.getName();
+        this.introduced = computer.getDateIntro().toString();
+        this.discontinued = computer.getDateDiscontinued().toString();
+        this.company = computer.getManufacturer();
+	}
+	
+	public ComputerDto(int id, String name, String date1, String date2, Company company) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.introduced = date1;
+        this.discontinued = date2;
+        this.company = company;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIntroduced() {
+		return introduced;
+	}
+
+	public void setIntroduced(String introduced) {
+		this.introduced = introduced;
+	}
+
+	public String getDiscontinued() {
+		return discontinued;
+	}
+
+	public void setDiscontinued(String discontinued) {
+		this.discontinued = discontinued;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+	
+}
---
+++ src/main/java/com/excilys/cdb/dto/DtoMapper.java
@@ -0,0 +1,50 @@
+package com.excilys.cdb.dto;
+
+import java.time.LocalDateTime;
+
+import com.excilys.cdb.cli.util;
+import com.excilys.cdb.model.Company;
+import com.excilys.cdb.model.Computer;
+
+public class DtoMapper {
+	
+	public DtoMapper(){
+		
+	}
+	
+    public static ComputerDto computerToDto(Computer computer) {
+    	
+    	String introduced="";
+    	String discontinued = "";
+        
+        int id = computer.getId();
+        String name = computer.getName();
+        if(computer.getDateIntro()!=null){
+        	introduced = computer.getDateIntro().toString();
+        }
+
+        if(computer.getDateDiscontinued()!=null){
+        	discontinued = computer.getDateDiscontinued().toString();
+        }
+        Company company = computer.getManufacturer();
+        
+        ComputerDto cdto = new ComputerDto(id, name, introduced, discontinued, company);
+        
+        return cdto;    
+        
+    }
+    
+    public static Computer dtoToComputer(ComputerDto computerd) {
+    	
+        int id = computerd.getId();
+        String name = computerd.getName();
+        LocalDateTime introduced = util.checkDate(computerd.getIntroduced());
+        LocalDateTime discontinued = util.checkDate(computerd.getDiscontinued());
+        Company company = computerd.getCompany();
+        
+        Computer c = new Computer(id, name, introduced, discontinued, company);
+        
+        return c;
+        
+        }
+}
