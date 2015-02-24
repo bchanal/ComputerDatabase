c4e3423066f49f9b7815d6560a5055b9b96f979e
bchanal <bchanal@excilys.com>
2015-02-24 17:18:53 +0100

    service exception

---
+++ src/main/java/com/excilys/cdb/exception/ServiceException.java
@@ -0,0 +1,30 @@
+package com.excilys.cdb.exception;
+
+public class ServiceException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ServiceException() {
+	}
+
+	public ServiceException(String message) {
+		super(message);
+	}
+
+	public ServiceException(Throwable cause) {
+		super(cause);
+	}
+
+	public ServiceException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ServiceException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
